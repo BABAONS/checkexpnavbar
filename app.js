e18cc71e389062805027a1d87a28babc10d48439
ONS BABA <nousababa72@gmail.com>
2024-07-23 18:47:39 +0200

    first commit

---
+++ app.js
@@ -0,0 +1,42 @@
+const express = require('express');
+const path = require('path');
+const port = 5000;
+
+const app = express();
+
+// Middleware to check if request is within working hours
+function workingHours(req, res, next) {
+    const now = new Date();
+    const day = now.getDay();
+    const hour = now.getHours();
+    if (day >= 1 && day <= 5 && hour >= 9 && hour <23) {
+        next();
+    } else {
+        res.send('The web application is only available Monday to Friday, from 9 to 17.');
+    }
+}
+
+// Set up view engine
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+
+// Middleware to serve static files
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Use the working hours middleware
+app.use(workingHours);
+
+// Define routes
+app.get('/', (req, res) => {
+    res.render('home');
+});
+
+app.get('/services', (req, res) => {
+    res.render('services');
+});
+
+app.get('/contact', (req, res) => {
+    res.render('contact');
+});
+//Running the server 
+app.listen(5000, () => console.log(`Server is running on http://localhost:${port}`));
